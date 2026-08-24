e68df6ea356caf9fb4c2fe13ea665c5f02dfc7f2
AIMAN NOMAN MOHAMMED AL ZEQRI <stum2013017017@gmail.com>
2026-08-24 23:05:40 +0530

    Add files via upload

--- array_2Duser.java
+++ array_2Duser.java
@@ -1,19 +1,31 @@
 import java.util.*;
-class array_2Duser{
+
+class array_2Duser {
     public static void main(String[] args) {
-        {
-            Scanner sc=new Scanner(System.in);
-            System.out.println("Enter size of array");
-            int n=sc.nextInt();
-            int a[]=new int[n];
-            System.out.println("Enter the element in the array");
-            for(int i=0;i<n;i++)
-            {
-                a[i]=sc.nextInt();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of rows");
+        int rows = sc.nextInt();
+
+        System.out.println("Enter number of columns");
+        int columns = sc.nextInt();
+
+        int a[][] = new int[rows][columns];
+
+        System.out.println("Enter the elements:");
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                a[i][j] = sc.nextInt();
             }
-            System.out.println("Element of array one");
-            for(int i=0;i<n;i++){
-                System.out.println(a[i]);
+        }
+
+        System.out.println("Elements of 2D array:");
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                System.out.println(a[i][j]);
             }
         }
     }
