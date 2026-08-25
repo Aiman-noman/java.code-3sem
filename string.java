424dbb08cfa5f09fcfce4025413e492bdbcc1e49
AIMAN NOMAN MOHAMMED AL ZEQRI <stum2013017017@gmail.com>
2026-08-25 09:22:40 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,15 @@
+class string{
+    public static void main(String[] args){
+        String text = "Helle Java";
+
+        System.out.println(text.length());
+        System.out.println(text.charAt(0));
+        System.out.println(text.substring(6));
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+        System.out.println(text.contains("Java"));
+        System.out.println(text.startsWith("Hello"));
+        System.out.println(text.endsWith("Java"));
+        System.out.println(text.indexOf("Java"));
+    }
+}
